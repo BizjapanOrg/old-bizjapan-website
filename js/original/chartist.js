fc8b79014a83a3be3cd14a63b9435482de434753
takurooper <taku.kuro@gmail.com>
2019-08-11 10:29:03 +0900

    edit-members-growth

--- js/original/chartist.js
+++ js/original/chartist.js
@@ -1,22 +1,36 @@
-var data = {
-    labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
-    series: [{
-        name: 'series-3',
-        data: [4, 31, 59, 90, 100, 145, 191, 216]
-                }]
-};
-var options = {
-    height: "100%",
-    width: "100%"
-};
-new Chartist.Line('.ct-chart-lines', data, options);
+var membergrowth = new Chartist.Line('.ct-chart-lines', {
+  labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
+  series: [{
+      name: 'series-3',
+      data: [4, 31, 59, 90, 100, 145, 191, 216]
+              }]
+},{
+  fullWidth: true,
+  lineSmooth: false,
+  chartPadding: {
+    right: 20,
+    left: 10
+  },
+  plugins: [
+    Chartist.plugins.tooltip()
+  ]
+});
 
 new Chartist.Pie('.ct-chart-pie-1',{
-    labels: ['ヨーロッパ', '台湾', 'その他', '日本', '中国', 'アメリカ', '韓国', 'シンガポール',],
-    series: [3, 3, 4, 174, 12, 5, 5, 5, ]
-    },options,{
+    labels: ['日本', '海外'],
+    series: [174, 37 ]
+    },{options},{
     labelOffset: 120,
     labelDirection: "explode"
+});
+
+new Chartist.Bar('.ct-chart-bar', {
+  labels: ['東京大学', '慶應義塾大学', '早稲田大学', '国際基督教大学', '上智大学', 'その他',],
+  series: [62.7, 17.6, 12.3, 3.4, 2.0, 2.0]
+  }, {
+  seriesBarDistance: 12,
+  low: -10,
+  high: 10
 });
 
 new Chartist.Pie('.ct-chart-pie-4',{
