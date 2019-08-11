ebb9bcf1fabe5bb81751f133aa12b2a416dbd2aa
takurooper <taku.kuro@gmail.com>
2019-08-11 11:48:06 +0900

    change graph color, change president picture

--- js/original/chartist.js
+++ js/original/chartist.js
@@ -1,4 +1,4 @@
-var membergrowth = new Chartist.Line('.ct-chart-lines', {
+var ct_member = new Chartist.Line('.ct-chart-lines', {
   labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
   series: [{
       name: 'series-3',
@@ -10,33 +10,26 @@ var membergrowth = new Chartist.Line('.ct-chart-lines', {
   chartPadding: {
     right: 20,
     left: 10
-  },
-  plugins: [
-    Chartist.plugins.tooltip()
-  ]
+  }
 });
 
-new Chartist.Pie('.ct-chart-pie-1',{
+var ct_nationality = new Chartist.Pie('.ct-chart-pie-nationality',{
     labels: ['日本', '海外'],
     series: [174, 37 ]
-    },{options},{
+    },{
+      fullWidth: true
+    },{
     labelOffset: 120,
     labelDirection: "explode"
 });
 
-new Chartist.Bar('.ct-chart-bar', {
+var ct_university = new Chartist.Bar('#ct-university', {
   labels: ['東京大学', '慶應義塾大学', '早稲田大学', '国際基督教大学', '上智大学', 'その他',],
-  series: [62.7, 17.6, 12.3, 3.4, 2.0, 2.0]
-  }, {
+  series: [[62.7, 17.6, 12.3, 3.4, 2.0, 2.0]]
+  },{
+    fullWidth: true
+  },{
   seriesBarDistance: 12,
   low: -10,
   high: 10
-});
-
-new Chartist.Pie('.ct-chart-pie-4',{
-    labels: ['上智大学', '国際基督教大学', 'その他','東京大学', '慶應義塾大学', '早稲田大学'],
-    series: [2.0, 3.4, 2.0, 62.7, 17.6, 12.3]
-    },options,{
-    labelOffset: 120,
-    labelDirection: "explode"
 });
