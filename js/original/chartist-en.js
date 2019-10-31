5c2f4fc21a4fc71d70898e2f4064bb6afd32ae46
hitomikuroki <47492485+hitomikuroki@users.noreply.github.com>
2019-10-31 14:02:49 +0900

    Solve #117 #115 #113 #111 #108 (#121)
    * change-report-pastannualreport-constract-and-president-photo-in-profilepage
    
    q
    Qchange-report-pastannualreport-constract-and-president-photo-in-profilepage:q
    
    * change-english-profile-and-report-latest-version-and-fix-font-size-for-media-on-profile-and-report-and-fix-link-contact-to-jump
    
    * change-gragh-on-profilepage-from-js-to-image
    
    * change-gragh-to-image-and-delete-jsgraph-and-create-pre.html-to-preserve-html-code
    
    * past-annual-description-fix-using-textalign-center

--- js/original/chartist-en.js
+++ js/original/chartist-en.js
@@ -1,57 +1,43 @@
-var ct_member = new Chartist.Line(
-  '.ct-chart-lines',
-  {
-    labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
-    series: [
-      {
+var ct_member = new Chartist.Line('.ct-chart-lines', {
+    labels: ['2012', '13', '14', '15', '16', '17', '18', '19'],
+    series: [{
         name: 'series-3',
         data: [4, 31, 59, 90, 100, 145, 191, 216]
-      }
-    ]
-  },
-  {
+                }]
+    },{
     fullWidth: true,
     lineSmooth: false,
     chartPadding: {
       right: 42,
       left: 10
     }
-  }
-);
-
-var ct_nationality = new Chartist.Pie(
-  '.ct-chart-pie-nationality',
-  {
-    labels: ['Japan', 'Oversea'],
-    series: [174, 37]
-  },
-  {
-    fullWidth: true
-  },
-  {
-    labelOffset: 120,
-    labelDirection: 'explode'
-  }
-);
-
-var ct_university = new Chartist.Bar(
-  '#ct-university',
-  {
-    labels: ['UTokyo', 'KeioU', 'WasedaU', 'ICU', 'SophiaU', 'Others'],
+  });
+  
+  var ct_nationality = new Chartist.Pie('.ct-chart-pie-nationality',{
+      labels: ['Japan', 'Oversea'],
+      series: [174, 37 ]
+      },{
+        fullWidth: true
+      },{
+      labelOffset: 120,
+      labelDirection: "explode"
+  });
+  
+  var ct_university = new Chartist.Bar('#ct-university', {
+    labels: ['UTokyo', 'Keio', 'Waseda', 'ICU', 'Sophia', 'Others',],
     series: [[62.7, 17.6, 12.3, 3.4, 2.0, 2.0]]
-  },
-  {
-    fullWidth: true,
-    chartPadding: {
-      bottom: 10
-    }
-  },
-  {
+    },{
+      fullWidth: true,
+      chartPadding: {
+        bottom:50,
+        top:50
+      }
+    },{
     seriesBarDistance: 12,
     low: -10,
     high: 10
-  },
-  {
-    labels: 10
-  }
-);
+    },{
+      labels:10
+  });
+  
+  
